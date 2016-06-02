d092aad60dec5a5b6d139183e8c35b75ab6f17a6
Mohsen Mirhoseini Argi <mohsen@karinaco.com>
2016-06-02 07:52:00 +0430

    added more Robolectric tests

--- app/src/main/java/com/mirhoseini/weatherapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/mirhoseini/weatherapp/ui/activity/MainActivity.java
@@ -92,8 +92,6 @@ public class MainActivity extends BaseActivity implements MainView, ForecastWeat
 
     @OnClick(R.id.go)
     public void submit(View view) {
-//        Answers.getInstance().logCustom(new CustomEvent("Pressed Go Button").putCustomAttribute("City Name", city.getText().toString()));
-
         //hide keyboard for better UX
         Utils.hideKeyboard(this, city);
 
@@ -277,12 +275,12 @@ public class MainActivity extends BaseActivity implements MainView, ForecastWeat
     public void showConnectionError() {
         Timber.d("Showing Connection Error Message");
 
+        errorContainer.setVisibility(View.VISIBLE);
+
         if (internetConnectionDialog != null)
             internetConnectionDialog.dismiss();
 
         internetConnectionDialog = Utils.showNoInternetConnectionDialog(this, false);
-
-        errorContainer.setVisibility(View.VISIBLE);
     }
 
     @Override
--- app/src/test/java/com/mirhoseini/weatherapp/ui/activity/MainActivityRobolectricTest.java
+++ app/src/test/java/com/mirhoseini/weatherapp/ui/activity/MainActivityRobolectricTest.java
@@ -5,9 +5,9 @@ import android.widget.TextView;
 
 import com.mirhoseini.weatherapp.BuildConfig;
 import com.mirhoseini.weatherapp.R;
+import com.mirhoseini.weatherapp.support.ShadowSnackbar;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,20 +16,24 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowToast;
 
+import static com.mirhoseini.weatherapp.support.Assert.assertAlertDialogIsShown;
+import static com.mirhoseini.weatherapp.support.Assert.assertSnackbarIsShown;
 import static com.mirhoseini.weatherapp.support.Assert.assertViewIsNotVisible;
 import static com.mirhoseini.weatherapp.support.Assert.assertViewIsVisible;
+import static com.mirhoseini.weatherapp.support.ResourceLocator.getString;
 import static com.mirhoseini.weatherapp.support.ViewLocator.getTextView;
 import static com.mirhoseini.weatherapp.support.ViewLocator.getView;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 
 /**
  * Created by Mohsen on 31/05/16.
  */
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@Config(constants = BuildConfig.class, sdk = 21, shadows = {ShadowSnackbar.class})
 public class MainActivityRobolectricTest {
 
     private MainActivity activity;
@@ -44,7 +48,8 @@ public class MainActivityRobolectricTest {
 
     @After
     public void tearDown() throws Exception {
-        activity = null;
+//        activity = null;
+//        activity.finish();
     }
 
     @Test
@@ -92,28 +97,57 @@ public class MainActivityRobolectricTest {
 
         TextView progressMessage = getTextView(activity, R.id.progress_message);
         assertViewIsVisible(progressMessage);
-        Assert.assertThat(TEST_TEXT, equalTo(progressMessage.getText().toString()));
+        assertThat(TEST_TEXT, equalTo(progressMessage.getText().toString()));
     }
 
     @Test
     public void testShowOfflineMessage() {
+        activity.showOfflineMessage();
 
+        assertSnackbarIsShown(R.string.offline_message);
     }
 
     @Test
     public void testShowExitMessage() {
+        activity.showExitMessage();
 
+        assertThat(getString(R.string.msg_exit), equalTo(ShadowToast.getTextOfLatestToast()));
     }
 
     @Test
     public void testShowConnectionError() {
+        activity.showConnectionError();
 
+        assertAlertDialogIsShown(R.string.no_connection_title, R.string.no_connection);
+
+        View errorContainer = getView(activity, R.id.error_container);
+        assertViewIsVisible(errorContainer);
     }
 
     @Test
-    public void testShowRetryMessage() {
+    public void testShowDoubleConnectionError() {
+        activity.showConnectionError();
+        activity.showConnectionError();
 
+        assertAlertDialogIsShown(R.string.no_connection_title, R.string.no_connection);
+
+        View errorContainer = getView(activity, R.id.error_container);
+        assertViewIsVisible(errorContainer);
     }
 
 
+    @Test
+    public void testShowRetryMessage() {
+        activity.showRetryMessage();
+
+        assertSnackbarIsShown(R.string.retry_message);
+    }
+
+    @Test
+    public void testOnDestroy() {
+//        activity.finish();
+        activity.onDestroy();
+
+        assertNull(activity.presenter);
+    }
 }
